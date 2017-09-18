1ab617422162a9bed779604a51d48c54a2ffd01a
qinh <hqin@umich.edu>
2017-09-17 23:11:02 -0400

    fixing hover for touch screens by disabling for small screens

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -1,35 +1,3 @@
-function watchForHover() {
-    var hasHoverClass = false;
-    var container = document.body;
-    var lastTouchTime = 0;
-
-    function enableHover() {
-        // filter emulated events coming from touch events
-        if (new Date() - lastTouchTime < 500) return;
-        if (hasHoverClass) return;
-
-        container.className += ' has-hover';
-        hasHoverClass = true;
-    }
-
-    function disableHover() {
-        if (!hasHoverClass) return;
-
-        container.className = container.className.replace(' hasHover', '');
-        hasHoverClass = false;
-    }
-
-    function updateLastTouchTime() {
-        lastTouchTime = new Date();
-    }
-
-    document.addEventListener('touchstart', updateLastTouchTime, true);
-    document.addEventListener('touchstart', disableHover, true);
-    document.addEventListener('mousemove', enableHover, true);
-
-    enableHover();
-}
-
 $(".subheader .design").on('click', function() {
     $(".subheader .art").removeClass("active");  
     $(this).addClass("active");
@@ -62,7 +30,7 @@ $(".subheader .art").on('click', function() {
 
 $(document).ready(function() {
     $("#art").hide();
-    watchForHover();
+    //$('.swipebox').swipebox();
 
 
     // GOOGLE ANALYTICS
